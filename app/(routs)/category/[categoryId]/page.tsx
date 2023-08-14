b0047262a49a2d135e67c4ccca94642c49758aaa
anatoliy <anatoliynbulyga@gmail.com>
2023-08-14 12:11:45 +0300

    added sort of products

--- app/(routs)/category/[categoryId]/page.tsx
+++ app/(routs)/category/[categoryId]/page.tsx
@@ -1,11 +1,13 @@
-
 import Container from "@/components/ui/container";
 import Billboard from "@/components/ui/billboard";
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product/product-list";
+import { ProductType } from "@/types";
+
 const HomePage = async () => {
-    const products = await getProducts({});
+    const featuredProducts = await getProducts({type: ProductType.Featured});
+    const trandingProducts = await getProducts({type: ProductType.Trending});
     const billboard = await getBillboard("1");
 
     return (
@@ -13,10 +15,10 @@ const HomePage = async () => {
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard} />
                 <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
+                    <ProductList title="Featured Products" items={featuredProducts} />
                 </div>
                 <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Tranding Products" items={products} />
+                    <ProductList title="Tranding Products" items={trandingProducts} />
                 </div>
             </div>
         </Container>
